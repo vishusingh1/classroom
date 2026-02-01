46bd97b68d464562263ace24bf24b5a3ef62ac69
Vishal singh <vishal@Vishals-MacBook-Air.local>
2026-02-01 13:07:05 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,21 @@ import routerProvider, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router";
-import { BrowserRouter, Route, Routes } from "react-router";
+import {BrowserRouter, Outlet, Route, Routes} from "react-router";
 import "./App.css";
 import { Toaster } from "./components/refine-ui/notification/toaster";
 import { useNotificationProvider } from "./components/refine-ui/notification/use-notification-provider";
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import { dataProvider } from "./providers/data";
+import Dashboard from "@/pages/dashboard.tsx";
+import {BookOpen, Home} from "lucide-react";
+import {Layout} from "@/components/refine-ui/layout/layout.tsx";
+import SubjectsList from "@/pages/subjects/list.tsx";
+import {SubjectsCreate} from "@/pages/subjects/create.tsx";
 
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
         <ThemeProvider>
           <DevtoolsProvider>
@@ -29,9 +33,36 @@ function App() {
                 warnWhenUnsavedChanges: true,
                 projectId: "6SLmOL-s6bvS1-J6RrHD",
               }}
+              resources={[
+                  {
+                      name: "dashboard",
+                      list: '/',
+                      meta:{ label: 'Home', icon: <Home />}
+                  },
+                  {
+                      name: "subjects",
+                      list: '/subjects',
+                      create: '/subjects/create',
+                      meta:{ label: 'Subjects', icon: <BookOpen />}
+
+                  }
+              ]}
             >
               <Routes>
-                <Route index element={<WelcomePage />} />
+                  <Route element={
+                      <Layout>
+                          <Outlet />
+                      </Layout>
+
+                  }>
+                      <Route path="/" element={<Dashboard />}/>
+                      <Route path={'/subjects'}>
+                          <Route index element={<SubjectsList />}/>
+                           <Route path={'create'} element={<SubjectsCreate />}/>
+
+                      </Route>
+                  </Route>
+
               </Routes>
               <Toaster />
               <RefineKbar />
